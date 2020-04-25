b620770dca64ee725b966722fdfbc3b458b3a0df
Ryan Van Etten <ryanve@gmail.com>
2020-04-24 22:18:41 -0400

    Update linting

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
 !function(root) {
-  var common = typeof module != 'undefined' && !!module.exports;
-  var actual = common ? require('./') : root.actual;
+  var common = typeof module != "undefined" && !!module.exports
+  var actual = common ? require("./") : root.actual
 
   function ok(result, correct) {
     if (result !== correct) {
-      throw new Error(result + ' should be ' + correct);
+      throw new Error(result + " should be " + correct)
     }
   }
 
-  ok(typeof actual('width'), 'number');
-  ok(typeof actual.mq(''), 'boolean');
-  ok(actual.is, actual.mq);
-  ok(actual.actual, actual);
-  ok(actual.as('px')('width'), actual('width', 'px'));
+  ok(typeof actual("width"), "number")
+  ok(typeof actual.mq(""), "boolean")
+  ok(actual.is, actual.mq)
+  ok(actual.actual, actual)
+  ok(actual.as("px")("width"), actual("width", "px"))
 
-  console.log('All tests passed =)');
-}(this);
+  console.log("All tests passed =)")
+}(this)
