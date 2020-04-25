e0d7bae065bf1363ee439257e0c2a69c4a49046c
Ryan Van Etten <ryanve@gmail.com>
2020-04-24 22:24:18 -0400

    Update linting (#5)

--- actual.js
+++ actual.js
@@ -1,7 +1,7 @@
 !function(root, name, make) {
-  if (typeof module != 'undefined' && module.exports) module.exports = make();
-  else root[name] = make();
-}(this, 'actual', function() {
+  if (typeof module != "undefined" && module.exports) module.exports = make()
+  else root[name] = make()
+}(this, "actual", function() {
 
   /**
    * @param {string} feature range feature name e.g. "width"
@@ -11,22 +11,22 @@
    * @return {number} breakpoint (0 if invalid unit or feature)
    */
   function actual(feature, unit, init, step) {
-    var up, gte, lte, curr, mq = actual['mq'];
-    unit = typeof unit == 'string' ? unit : '';
-    init = 0 < init ? (unit ? +init : init>>0) : 1;
+    var up, gte, lte, curr, mq = actual["mq"]
+    unit = typeof unit == "string" ? unit : ""
+    init = 0 < init ? unit ? +init : init>>0 : 1
     // Step starts positive. Minimize iterations, unless feat may be "integer" type.
-    step = 0 < step ? +step : 0 > step ? -step : 'px' == unit ? 256 : unit ? 32 : 1;
-    for (feature += ':', unit += ')', curr = init; step && 0 <= curr; curr+=step) {
-      lte = mq('(min-' + feature + curr + unit);
-      gte = mq('(max-' + feature + curr + unit);
+    step = 0 < step ? +step : 0 > step ? -step : "px" == unit ? 256 : unit ? 32 : 1
+    for (feature += ":", unit += ")", curr = init; step && 0 <= curr; curr+=step) {
+      lte = mq("(min-" + feature + curr + unit)
+      gte = mq("(max-" + feature + curr + unit)
       // Found: Use the floored value if it makes an exact match. Else return as is.
-      if (lte && gte) return mq('(' + feature + (curr>>0) + unit) ? curr>>0 : curr;
+      if (lte && gte) return mq("(" + feature + (curr>>0) + unit) ? curr>>0 : curr
       // 1st iteration: Save direction. Flip if down. Break if neither b/c unknown.
-      if (null == up) step = (up = !gte) ? lte && step : -step;
+      if (null == up) step = (up = !gte) ? lte && step : -step
       // Later iterations: If skipped, reverse direction and raise precision.
-      else if (gte ? up : !up) up = !up, step = -step/2;
+      else if (gte ? up : !up) up = !up, step = -step/2
     }
-    return 0;
+    return 0
   }
 
   /**
@@ -35,18 +35,19 @@
    */
   function as(unit) {
     return function(feature) {
-      return actual(feature, unit);
-    };
+      return actual(feature, unit)
+    }
   }
 
-  var media = 'matchMedia', win = typeof window != 'undefined' && window;
-  actual['actual'] = actual;
-  actual['as'] = as;
-  actual['is'] = actual['mq'] = win[media] || win[media = 'msMatchMedia'] ? function(q) {
-    return !!win[media](q).matches;
+  var media = "matchMedia"
+  var win = typeof window != "undefined" && window
+  actual["actual"] = actual
+  actual["as"] = as
+  actual["is"] = actual["mq"] = win[media] || win[media = "msMatchMedia"] ? function(q) {
+    return !!win[media](q).matches
   } : function() {
-    return false;
-  };
+    return false
+  }
 
-  return actual;
-});
+  return actual
+})
